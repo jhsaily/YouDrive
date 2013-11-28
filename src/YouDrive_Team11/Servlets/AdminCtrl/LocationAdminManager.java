1ba35a950a9152ebb9444e67991a2132f5ee385c
Shooflower <tanya27@uga.edu>
2013-11-27 21:13:51 -0500

    Edited AdminCtrl

--- src/YouDrive_Team11/Servlets/AdminCtrl/LocationAdminManager.java
+++ src/YouDrive_Team11/Servlets/AdminCtrl/LocationAdminManager.java
@@ -1,10 +1,30 @@
 package YouDrive_Team11.Servlets.AdminCtrl;
 
+import java.util.LinkedList;
+
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+
+import YouDrive_Team11.Persistence.*;
+import YouDrive_Team11.Entity.*;
 
 public class LocationAdminManager extends HttpServlet{
+	/**
+	 * Variables
+	 */
+	
+	//Declare session object
+	HttpSession session;
+	
+	//Declare DAO
+	YouDriveDAO dao;
+	
+	//Declare temporary customer object
+	Vehicle vehicle;
+	
 	/**
 	 * Constructor
 	 */
@@ -18,5 +38,21 @@ public class LocationAdminManager extends HttpServlet{
 	
 	public void doPost(HttpServletRequest req, HttpServletResponse res){
 		
+	}
+	
+	public RentalLocation addRentalLocation(String name, int capacity, String addrLine1, String addrLine2, String city, String state, int zip, String country){
+		return dao.createRentalLocation(name, capacity, addrLine1, addrLine2, city, state, zip, country);
+	}
+	
+	public void editRentalLocation(int locationId, String name, int capacity){
+		dao.updateRentalLocation(locationId, name, capacity);
+	}
+	
+	public void removeRentalLocation(int locationId){
+		dao.deleteRentalLocation(locationId);
+	}
+	
+	public LinkedList getAllLocations(){
+		return dao.getAllRentalLocations();
 	}
 }
--- src/YouDrive_Team11/Servlets/AdminCtrl/VehicleAdminManager.java
+++ src/YouDrive_Team11/Servlets/AdminCtrl/VehicleAdminManager.java
@@ -1,17 +1,37 @@
 package YouDrive_Team11.Servlets.AdminCtrl;
 
+import java.sql.Date;
 import java.util.Currency;
+import java.util.LinkedList;
 
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import YouDrive_Team11.Entity.*;
+import YouDrive_Team11.Persistence.*;
 
 public class VehicleAdminManager extends HttpServlet {
+	/**
+	 * Variables
+	 */
+	
+	//Declare session object
+	HttpSession session;
+	
+	//Declare DAO
+	YouDriveDAO dao;
+	
+	//Declare temporary customer object
+	Vehicle vehicle;
+	
 	/**
 	 * Constructor
 	 */
 	public VehicleAdminManager(){
 		super();
+		dao=new YouDriveDAO();
 	}
 	
 	public void doGet(HttpServletRequest req, HttpServletResponse res){
@@ -24,5 +44,30 @@ public class VehicleAdminManager extends HttpServlet {
 	
 	public void addVehicleType(String description, Currency hourlyRate, Currency dailyRate){
 		
+	}
+	
+	public void editVehicleType(int vehicleTypeId, String description, Currency hourlyRate, Currency dailyRate){
+		
+	}
+	
+	public void removeVehicleType(int vehicleTypeId){
+		
+	}
+	
+	public Vehicle addVehicle(String make, String model, int year, String tag, int mileage, Date serviceDate, String condition, int type, int location){
+		Vehicle vehicle=dao.createVehicle(make, model, year, tag, mileage, serviceDate, condition, type, location);
+		return vehicle;
+	}
+	
+	public void editVehicle(int vehicleId, String make, String model, int year, String tag, int mileage, Date serviceDate, String condition, int type, int location){
+		dao.updateVehicle(vehicleId, make, model, year, tag, mileage, serviceDate, condition, type, location);
+	}
+	
+	public void removeVehicle(int vehicleId){
+		dao.deleteVehicle(vehicleId);
+	}
+	
+	public LinkedList<Vehicle> getAllVehicles(){
+		return dao.getAllVehicles();
 	}
 }
